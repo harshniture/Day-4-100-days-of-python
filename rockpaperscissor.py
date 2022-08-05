160ed660c155f6c0af63381f1db740721011819e
harshniture <harshsnitures@gmail.com>
2022-08-06 01:28:42 +0530

    Day 4 program optimized

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -7,37 +7,18 @@ print("Welcome to Rock Paper Scissor Game ! ")
 a = int(input("What do you wamt to choose? 1 for Rock 2 for Paper 3 for Scissor: "))
 b = random.randint(1,3)
 
-if a == 1:
-    print("You chose Rock")
-    if b == 1:
-        print("Computer chose Rock")
-        print("It's a tie")
-    elif b == 2:
-        print("Computer chose Paper")
-        print("You lose")
-    else:
-        print("Computer chose Scissor")
-        print("You win")
-elif a == 2:
-    print("You chose Paper")
-    if b == 1:
-        print("Computer chose Rock")
-        print("You win")
-    elif b == 2:
-        print("Computer chose Paper")
-        print("It's a tie")
-    else:
-        print("Computer chose Scissor")
-        print("You lose")
-elif a == 3:
-    print("You chose Scissor")
-    if b == 1:
-        print("Computer chose Rock")
-        print("You lose")
-    elif b == 2:
-        print("Computer chose Paper")
-        print("You win")
-    else:
-        print("Computer chose Scissor")
-        print("It's a tie")
-    
+c = ["Rock", "Paper", "Scissor"]
+
+print("you choose: " + c[a-1])
+print("computer choose: " + c[b-1])
+
+if a == b:
+    print("It's a tie!")
+elif a > b:
+    print("You win!")
+elif a == 1 and b == 3:
+    print("You win!")
+elif a < b:
+    print("You lose!")
+else:
+    print("Invalid input , You lose !")
